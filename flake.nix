36d20cbc62b2e88b1d93a8fac7ff7bb411caf72c
liyankova <liyannkova@gmail.com>
2025-09-17 08:42:38 +0700

    change flake.nix to be dual mode

--- flake.nix
+++ flake.nix
@@ -11,12 +11,17 @@
       url = "github:tadfisher/android-nixpkgs/stable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, home-manager, android-nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, android-nixpkgs, flake-utils, ... }@inputs:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      overlays = [ ];
+      pkgs = import nixpkgs { 
+        inherit system overlays; 
+        config.allowUnfree = true;
+      };
       # pkgs = nixpkgs.legacyPackages.${system}; 
 #      dotfiles = /home/liyan/.dotfiles;
     in
@@ -24,19 +29,20 @@
       nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
         inherit system;
 	specialArgs = {                 
-           inherit android-nixpkgs;
-	   inherit self;
+           inherit android-nixpkgs system inputs;
+	   # specialArgs = { inherit inputs; };
         };
         modules = [
 	  ./hosts/laptop/host.nix
-	  home-manager.nixosModules.home-manager
-	  {
-	    home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
-	    home-manager.users.liyan = import ./home/liyan/home.nix;
-          }
+	  # ./nix/modules/system/system-core.nix
+	  # home-manager.nixosModules.home-manager
+	  # {
+	  #   home-manager.useGlobalPkgs = true;
+	  #   home-manager.useUserPackages = true;
+	  #   home-manager.users.liyan = import ./home/liyan/home.nix;
+	  #        }
         ];
-      };
+      
 
 	#      homeConfigurations.liyan = home-manager.lib.homeManagerConfiguration {
 	#        inherit pkgs;
@@ -53,4 +59,25 @@
 #  extraSpecialArgs = { inherit dotfiles; }; 
 #      };
     };
+          # ==============================
+      # 2. Home Manager Configurations
+      # ==============================
+      homeConfigurations = {
+        # Jalankan di NixOS atau Debian/Fedora/Arch (non-NixOS)
+        "liyan@laptop" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home/liyan/home.nix
+          ];
+        };
+
+        # contoh lain kalau kamu install di Debian
+        "liyan@debian" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home/liyan/home.nix
+          ];
+        };
+      };
+      };
 }
